8d283b918c1aa77a0c3d7a7e8b38f29e54efbdcf
kyle mcarthur <teerzo@live.com.au>
2023-07-06 22:09:52 +1000

    Fixed login layout error

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,11 @@
+
+import Link from "next/link";
+
+export default function LoginLayout({ children, }: { children: React.ReactNode }) {
+    return (
+        <section>
+            {/* Include shared UI here e.g. a header or sidebar */}
+            {children}
+        </section>
+    )
+}
